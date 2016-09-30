a05de41e099991fc9056b4d42cdd4c1c69f0d7dc
Hamilton <hrszanini@gmail.com>
2016-09-30 20:23:57 -0300

    Classe Estatistica em processo de criação

--- ocorrenciaCrime/src/view/Teste.java
+++ ocorrenciaCrime/src/view/Teste.java
@@ -1,10 +1,15 @@
 package view;
 
+import model.BancoDeDados;
+import model.Estatistica;
 import model.TipoOcorrencia;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		System.out.println(TipoOcorrencia.ASSALTO.getDescricao());
+		
+		BancoDeDados b = new BancoDeDados();
+		Estatistica e = new Estatistica(b.getListOcorrencias(),b.getListUsuarios());
 	}
 }
