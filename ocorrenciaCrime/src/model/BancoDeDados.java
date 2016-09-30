8820a452c237cc873d7d26eda9fcf08398070dcd
Giuliano Gimenez <ggimenez87@gmail.com>
2016-09-30 19:54:50 -0300

    BancoDeDados com atributos.

--- ocorrenciaCrime/src/model/BancoDeDados.java
+++ ocorrenciaCrime/src/model/BancoDeDados.java
@@ -1,7 +1,43 @@
 package model;
 
-import java.awt.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BancoDeDados {
+	private List<Ocorrencia> listOcorrencias = new ArrayList();
+	private List<Usuario> listUsuarios = new ArrayList();
+	//TODO: private List<Estatistica> listEstatisticas = new ArrayList();
 	
+	public List<Ocorrencia> getListOcorrencias() {
+		return listOcorrencias;
+	}
+	public void setListOcorrencias(List<Ocorrencia> listOcorrencias) {
+		this.listOcorrencias = listOcorrencias;
+	}
+	public List<Usuario> getListUsuarios() {
+		return listUsuarios;
+	}
+	public void setListUsuarios(List<Usuario> listUsuarios) {
+		this.listUsuarios = listUsuarios;
+	}
+	
+	public List<Ocorrencia> listarOcorrencia(LocalSpec localSpec) {
+		List<Ocorrencia> ocorrenciasEncontradas = new ArrayList();
+		for (Ocorrencia ocorrencia : listOcorrencias) {
+			if(ocorrencia.getLocal().getLocalSpec().match(localSpec)) {
+				ocorrenciasEncontradas.add(ocorrencia);
+			}
+		}
+		return ocorrenciasEncontradas;
+	}
+	
+	public List<Ocorrencia> listarOcorrencia(Usuario usuario) {
+		List<Ocorrencia> ocorrenciasEncontradas = new ArrayList();
+		for (Ocorrencia ocorrencia : listOcorrencias) {
+			if(ocorrencia.getEmailUsuario().equals(usuario.getEmail())) {
+				ocorrenciasEncontradas.add(ocorrencia);
+			}
+		}
+		return ocorrenciasEncontradas;
+	}
 }
