f7e2e27ac42372eb5670ce0599e9d248a5039c99
Giuliano Gimenez <ggimenez87@gmail.com>
2016-09-30 19:37:32 -0300

    Adição da classe BancoDeDados

---
+++ ocorrenciaCrime/src/model/BancoDeDados.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.awt.List;
+
+public class BancoDeDados {
+	
+}
--- ocorrenciaCrime/src/view/Teste.java
+++ ocorrenciaCrime/src/view/Teste.java
@@ -1,10 +1,10 @@
 package view;
 
+import model.TipoOcorrencia;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println(TipoOcorrencia.ASSALTO.getDescricao());
 	}
-
 }
